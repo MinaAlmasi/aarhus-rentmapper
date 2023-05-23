b31e7a71125da10a20c9ee9a8188441e02eb049c
MinaAlmasi <202005465@post.au.dk>
2023-05-23 10:21:18 +0200

    add data_clean for boligportal

---
+++ src/data-clean/boligportal.py
@@ -0,0 +1,56 @@
+# utils 
+import pathlib 
+import re 
+
+# data wrangling
+import pandas as pd
+
+def main():
+    # path to file 
+    path = pathlib.Path(__file__)
+
+    # path to data folder
+    rawdata_path = path.parents[2] / "data" / "raw_data"
+
+    # read in data
+    df = pd.read_csv(rawdata_path / "boligportal_aarhus.csv")
+
+    # add website column
+    df["website"] = "boligportal.dk"
+
+    # add year column
+    df["year"] = 2023
+
+    # exract rental type from rooms_type_kvm column
+    df['rental_type'] = df["rooms_type_kvm"].str.extract(r'· ([\w\s]+) ·')
+
+    # extract number of rooms from rooms_type_kvm column
+    df["rooms"] = df["rooms_type_kvm"].str.extract(r'(\d+) vær\.')
+
+    # extract size from rooms_type_kvm column
+    df['square_meters'] = df["rooms_type_kvm"].str.extract(r'(\d+) m²')
+
+    # translate "Lejlighed" to "apartment" and "Værelse" to "room"
+    df["rental_type"] = df["rental_type"].str.replace("Lejlighed", "apartment")
+    df["rental_type"] = df["rental_type"].str.replace("Værelse", "room")
+
+    # remove kr on price
+    df["price"] = df["price"].str.replace("kr.", "")
+
+    # rename price to rent_without_expenses
+    df = df.rename(columns={"price": "rent_without_expenses"})
+    
+    # 
+    df[""]
+
+    # print cols 
+    print(df.columns)
+
+    # select cols
+    df = df[["website", "year", "rental_type", "rent_without_expenses", "square_meters", "zip_code", "street", "address"]]
+    
+
+    print(df)
+
+if __name__ == "__main__":
+    main()
