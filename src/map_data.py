9d99570fee82014df3e0d2602b3b84e8c1e78a33
Anton Drasbæk <202008161@post.au.dk>
2023-05-25 15:30:05 +0200

    working plot

--- src/map_data.py
+++ src/map_data.py
@@ -5,6 +5,7 @@ Script to map Aarhus data.
 import geopandas as gpd
 import pandas as pd
 import pathlib
+import matplotlib.pyplot as plt
 
 
 
@@ -13,19 +14,28 @@ def main():
     # define paths
     path = pathlib.Path(__file__)
 
-    complete_data = pd.read_csv(path.parents[1] / 'data' / 'complete_data.csv')
+    # read in data
+    district_data = pd.read_csv(path.parents[1] / "data" / "district_aggregates.csv")
 
-    
-
-    # drop "geometry_street"
-    complete_data = complete_data.drop(columns=['geometry_street'])
+    # change wkt to geometry
+    district_data["geometry"] = gpd.GeoSeries.from_wkt(district_data["geometry"])
 
     # convert to geodataframe
-    complete_data = gpd.GeoDataFrame(complete_data)
+    district_data = gpd.GeoDataFrame(district_data, geometry="geometry")
+
+    # set epsg to 25832
+    district_data = district_data.set_crs(epsg=25832)
+
+    # plot apartment rent per square meter in 2023
+    fig, ax = plt.subplots(figsize=(30, 30))
+
+    district_data.plot(column="apartment_rent_sqm_now", ax=ax, legend=True, legend_kwds={"label": "Apartment rent per square meter in 2023", "orientation": "horizontal"})
+
+    ax.set_title("Apartment rent per square meter in 2023", fontsize=20)
+
+    plt.savefig(path.parents[1] / "apartment_rent_sqm_now.png")
 
     
-
-
 
 if __name__ == "__main__":
     main()
