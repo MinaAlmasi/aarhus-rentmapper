c524125237531b61f16a1abb6a15f37d0554dbbb
Anton Drasbæk <202008161@post.au.dk>
2023-05-27 14:54:10 +0200

    added streetview

--- src/app.py
+++ src/app.py
@@ -1,33 +1,23 @@
+'''
+Streamlit app
+'''
+# utils 
+import pathlib
+
+# web app
 import streamlit as st
-import geopandas as gpd
+
+# geospatial mapping
 import folium
 from streamlit_folium import folium_static
-import pandas as pd
-import pathlib
 from folium.features import GeoJsonPopup, GeoJsonTooltip
 
-def add_Erhvervshavnen():
-    path = pathlib.Path(__file__)
+# visualisations
+import plotly.graph_objects as go
 
-    # read in geojson
-    geojson = gpd.read_file(path.parents[1] / "utils" / "districts.geojson")
-
-    # set crs to 25832
-    geojson = geojson.set_crs("epsg:25832")
-
-    # extract "Erhvervshavnen"
-    Erhvervshavnen = geojson[geojson["prog_distrikt_navn"] == "Erhvervshavnen"]
-
-    # rename prog_distrikt_navn to district
-    Erhvervshavnen = Erhvervshavnen.rename(columns={"prog_distrikt_navn": "district"})
-
-    # remove all columns but district and geometry
-    Erhvervshavnen = Erhvervshavnen[["district", "geometry"]]
-
-    # convert to geodataframe
-    Erhvervshavnen = gpd.GeoDataFrame(Erhvervshavnen, geometry="geometry")
-
-    return Erhvervshavnen
+# data wrangling 
+import pandas as pd
+import geopandas as gpd
 
 def add_missing_districts():
     path = pathlib.Path(__file__)
@@ -51,13 +41,7 @@ def add_missing_districts():
 
     return missing_districts
 
-#def style_statistics
-
-
-def main():
-    # define paths
-    path = pathlib.Path(__file__)
-
+def district_view(path): 
     # read in data
     data = pd.read_csv(path.parents[1] / "data" / "district_aggregates.csv")
 
@@ -75,15 +59,6 @@ def main():
 
     # add missing districts to dataframe
     missing_districts = add_missing_districts()
-
-    # make layout wide 
-    st.set_page_config(layout="wide")
-
-    # title of dashboard
-    st.write("<style>.block-container {padding-top: 0 !important;}</style>", unsafe_allow_html=True) # remove padding
-    st.markdown("<h1 style='text-align: center;'>Aarhus Apartment Rent</h1>", unsafe_allow_html=True)
-    st.divider()
-
 
     # create columns for map and statistics
     col1, col2, = st.columns(2)
@@ -157,14 +132,14 @@ def main():
         style_function=lambda x: {"color": "#FF595A", "weight": 4, "opacity": 1, "fillOpacity": 0},
         ).add_to(folium_map)
 
-        folium_static(folium_map)
+        folium_static(folium_map, height=475)
     
     with col1:
         # create columns to display statistics
         st.markdown(f"<h3>Rental Statistics for <span style='color: #FF595A;'>{selected_district}</span></h3>", unsafe_allow_html=True)
 
         # create subsubheader for apartment rent
-        st.write("Average Apartment Rent (per m2)")
+        st.write("__Average Apartment Rent (per m2)__")
 
         apart_col1, apart_col2, apart_col3 = st.columns(3)
 
@@ -177,7 +152,7 @@ def main():
         # add spacing
         st.write("")
 
-        # create the same for room rent
+        # create the same for room rent  
         st.write("__Average Room Rent__")
 
         room_col1, room_col2, room_col3 = st.columns(3)
@@ -188,10 +163,172 @@ def main():
         with room_col2:
             st.metric(label="in 2014-2016", value=f"{selected_data['room_rent_then'].values[0]} DKK")
     
-
-
     # layer control for street view
     folium.LayerControl().add_to(folium_map)
+
+def street_view(path):
+    # read in data
+    street_data = pd.read_csv(path.parents[1] / "data" / "street_aggregates.csv")
+
+    # change wkt to geometry
+    street_data["geometry"] = gpd.GeoSeries.from_wkt(street_data["geometry_street"])
+
+    # convert to geodataframe
+    street_data = gpd.GeoDataFrame(street_data, geometry="geometry")
+
+    # set epsg to 25832
+    street_data = street_data.set_crs("epsg:25832")
+
+    # create columns for map and statistics
+    col1, col2, = st.columns(2)
+
+    with st.sidebar:
+        selected_street = st.selectbox('Select a street', street_data['street'])
+        selected_data = street_data[street_data['street'] == selected_street]
+
+        st.write(f"{len(street_data)} streets found")
+
+        # convert to epsg 4326
+        selected_data = selected_data.to_crs("epsg:4326")
+
+        # extract location coordinates
+        selected_location = [selected_data['geometry'].centroid.y, selected_data['geometry'].centroid.x]
+
+    with col2:
+        # add spacing
+        st.write("")
+    
+        # update center of map to selected district
+        folium_map = folium.Map(location=selected_location,
+                                zoom_start=15,
+                                min_zoom=10)
+        
+        # define tooltip, but unclickable
+        tooltip = GeoJsonTooltip(
+            fields=['street'], 
+            aliases=['Street: '],
+            labels=True,
+            permanent=False
+        )
+        
+        # create map with location being selected_location, but all strets highlighted
+        folium.GeoJson(street_data,
+            tooltip=tooltip,
+            style_function=lambda x: {"color": "#001233", "weight": 3, "opacity": 1, "fillOpacity": 0},
+        ).add_to(folium_map)
+
+        # add selected district to map
+        folium.GeoJson(selected_data, 
+        style_function=lambda x: {"color": "#FF595A", "weight": 5, "opacity": 1, "fillOpacity": 0},
+        ).add_to(folium_map)
+
+        folium_static(folium_map, height=475, width=500)
+    
+    with col1:
+        # create columns to display statistics
+        st.markdown(f"<h3>Rental Statistics for <span style='color: #FF595A;'>{selected_street}</span></h3>", unsafe_allow_html=True)
+
+        # create small text for "located in" 
+        st.markdown(f"<body><b>Located in <span style='color: #FF595A;'>{selected_data['district'].values[0]}</b></span></body>", unsafe_allow_html=True)
+
+        # add spacing
+        st.write("")
+        st.write("")
+
+        # create columns for rent statistics
+        st.write("__Average Apartment Rent (per m2)__")
+
+        col1, col2 = st.columns([2, 2])
+
+        with col1:
+            st.metric(label="in 2023", value=f"{selected_data['rent_per_square_meter'].values[0]} DKK")
+        
+        with col2:
+            st.metric(label="based on", value=f"{selected_data['count'].values[0]} apartments")
+        
+        # add spacing
+        st.write("")
+        st.write("")
+
+        with st.container():
+            st.write("__Similar Priced Streets__")
+
+            # add DKK to rent in selected data in most_similar cols 
+            for i in range(1, 6):
+                selected_data[f"most_similar_rent_{i}"] = selected_data[f"most_similar_rent_{i}"].astype(str) + " DKK"
+
+            # define headers and row values for table
+            header = ["<b>Street</b>", "<b>District</b>", "<b>Rent (per m2)</b>"]
+
+            values = [  # streetname 
+                        [selected_data["most_similar_1"], selected_data["most_similar_2"], 
+                        selected_data["most_similar_3"], selected_data["most_similar_4"],
+                        selected_data["most_similar_5"]],
+                        # district
+                        [selected_data["most_similar_district_1"], selected_data["most_similar_district_2"],
+                        selected_data["most_similar_district_3"], selected_data["most_similar_district_4"],
+                        selected_data["most_similar_district_5"]],
+                        # rent
+                        [selected_data["most_similar_rent_1"], selected_data["most_similar_rent_2"],
+                        selected_data["most_similar_rent_3"], selected_data["most_similar_rent_4"],
+                        selected_data["most_similar_rent_5"]]]
+       
+            # create table
+            fig = go.Figure(data=[go.Table(header=dict(values=header, 
+                                                       fill_color="#FF595A", 
+                                                       line_color="#FF595A", 
+                                                       align=["left", "left", "center"],
+                                                       height=30, 
+                                                       font = dict(size=16, family="Serif")), 
+                                           cells=dict(values=values, 
+                                                      fill_color="#001233", 
+                                                      line_color="#001233", 
+                                                      font = dict(size=14, family="Serif"), 
+                                                      align=["left", "left", "center"], 
+                                                      height=30)
+                                           )
+                        ])
+            
+            # fix weird padding
+            fig.update_layout(margin=dict(l=0, r=0, t=0, b=0),
+                              height=200,
+                              width=500)
+
+            # display table in streamlit
+            st.write(fig)
+        
+        
+
+
+
+
+        
+
+
+
+def main():
+    # define paths
+    path = pathlib.Path(__file__)
+
+    # make layout wide 
+    st.set_page_config(layout="wide")
+
+    # title of dashboard
+    st.write("<style>.block-container {padding-top: 0 !important;}</style>", unsafe_allow_html=True) # remove padding
+    st.markdown("<h1 style='text-align: center;'>Aarhus Rental Properties</h1>", unsafe_allow_html=True)
+    st.divider()
+
+    # add district view 
+    with st.sidebar:
+        view = st.radio("Select view", ("District", "Street"))
+
+    if view == "District":
+        district_view(path)
+    
+    if view == 'Street':
+        street_view(path)
+
+    
 
 if __name__ == '__main__':
     main()
