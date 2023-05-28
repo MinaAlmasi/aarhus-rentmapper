855e4423251a806e95e22e246d879497acfec86a
Anton Drasbæk <202008161@post.au.dk>
2023-05-28 13:28:13 +0200

    first commit

---
+++ app/district_view.py
@@ -0,0 +1,34 @@
+import pathlib
+import geopandas as gpd
+
+
+def add_missing_districts(path: pathlib.Path):
+    '''
+    Function for adding missing districts to dataframe.
+    Districts are missing because they have no apartment data.
+
+    Args:
+        path: path to script
+
+    Returns:
+        missing_districts: geodataframe with missing districts
+    '''
+    # read in geojson file
+    geojson = gpd.read_file(path.parents[1] / "utils" / "districts.geojson")
+
+    # set crs to 25832
+    geojson = geojson.set_crs("epsg:25832")
+
+    # extract "Erhvervshavnen" and "Sydhavnen og Marselisborg lystbådehavn" in one dataframe
+    missing_districts = geojson[(geojson["prog_distrikt_navn"] == "Erhvervshavnen") | (geojson["prog_distrikt_navn"] == "Sydhavnen og Marselisborg lystbådehavn")]
+
+    # rename prog_distrikt_navn to district
+    missing_districts = missing_districts.rename(columns={"prog_distrikt_navn": "district"})
+
+    # remove all columns but district and geometry
+    missing_districts = missing_districts[["district", "geometry"]]
+
+    # convert to geodataframe
+    missing_districts = gpd.GeoDataFrame(missing_districts, geometry="geometry")
+
+    return missing_districts
