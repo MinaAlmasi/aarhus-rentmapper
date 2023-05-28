34667b2e660a3ff6b25425f6a7809f4e706f41b3
Anton Drasbæk <202008161@post.au.dk>
2023-05-28 13:10:25 +0200

    150 px

--- app/app.py
+++ app/app.py
@@ -408,8 +408,8 @@ def main():
                 """
             <style>
             [data-testid="stSidebar"][aria-expanded="true"]{
-                min-width: 200px;
-                max-width: 200px;
+                min-width: 150px;
+                max-width: 150px;
             }
             """,
         unsafe_allow_html=True,
