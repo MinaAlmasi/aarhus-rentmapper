7ba965504af704cad07c7467b39ac46aa47a01ef
Anton Drasbæk <202008161@post.au.dk>
2023-05-29 16:39:55 +0200

    python -> python3.9

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # create virtual environment called env
-python3 -m venv env
+python3.9 -m venv env
 
 # activate virtual environment
 source ./env/bin/activate
 
 # install requirements
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
