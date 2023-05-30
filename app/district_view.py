85429efc02ea526d8674b9c380154bcb3e213c91
MinaAlmasi <202005465@post.au.dk>
2023-05-30 11:49:30 +0200

    missing districts in utils script

--- app/district_view.py
+++ app/district_view.py
@@ -31,36 +31,10 @@ import plotly.express as px
 import pandas as pd
 import geopandas as gpd
 
-def add_missing_districts(path: pathlib.Path):
-    '''
-    Function for adding missing districts to dataframe.
-    Districts are missing because they have no apartment data.
-
-    Args:
-        path: path to script
-
-    Returns:
-        missing_districts: geodataframe with missing districts
-    '''
-    # read in geojson file
-    geojson = gpd.read_file(path.parents[1] / "utils" / "districts.geojson")
-
-    # set crs to 25832
-    geojson = geojson.set_crs("epsg:25832")
-
-    # extract "Erhvervshavnen" and "Sydhavnen og Marselisborg lystbådehavn" in one dataframe
-    missing_districts = geojson[(geojson["prog_distrikt_navn"] == "Erhvervshavnen") | (geojson["prog_distrikt_navn"] == "Sydhavnen og Marselisborg lystbådehavn")]
-
-    # rename prog_distrikt_navn to district
-    missing_districts = missing_districts.rename(columns={"prog_distrikt_navn": "district"})
-
-    # remove all columns but district and geometry
-    missing_districts = missing_districts[["district", "geometry"]]
-
-    # convert to geodataframe
-    missing_districts = gpd.GeoDataFrame(missing_districts, geometry="geometry")
-
-    return missing_districts
+# custom module for adding missing districts
+import sys
+sys.path.append(str(pathlib.Path(__file__).parents[1] / "src"))
+from utils import add_missing_districts 
 
 def plot_neighbor_stats(neighbor_data, y_col, y_title):
         '''
