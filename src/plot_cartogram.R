07b6b76d5e5f98285d3814150a2e59ee20494a5a
Anton Drasbæk <202008161@post.au.dk>
2023-05-30 10:00:38 +0200

    fixed plot, made pretty, added normal map next to

--- src/plot_cartogram.R
+++ src/plot_cartogram.R
@@ -1,23 +1,22 @@
-'''
-R script to create cartogram plot of apartment rent in Aarhus based on districts.
-The output is saved to plots/cartogram_plot.jpg.
 
-Requires the package "pacman" to be installed. This can be achieved as such from your R console:
-  install.packages("pacman")
+# R script to create cartogram plot of apartment rent in Aarhus based on districts.
+# The output is saved to plots/cartogram_plot.jpg.
 
-by Anton Drasbæk Schiønning (@drasbaek) and Mina Almasi (@MinaAlmasi)
-Spatial Analytics, Cultural Data Science (F2023)
-'''
+# Requires the package 'pacman' to be installed. This can be achieved as such from your R console:
+    # install.packages('pacman')
+
+# by Anton Drasbæk Schiønning (@drasbaek) and Mina Almasi (@MinaAlmasi)
+# Spatial Analytics, Cultural Data Science (F2023)
 
 
 # install/load packages
-pacman::p_load(tidyverse, sf, cartogram)
+pacman::p_load(tidyverse, sf, cartogram, cowplot)
 
 # load district data
 data <- st_read(file.path("..", "data", "district_aggregates.csv"))
 
 # transform to geodata using SF
-geodata <- st_as_sf(data, wkt="geometry")
+geodata <- st_as_sf(data, wkt = "geometry")
 
 # ensure rent is numeric
 geodata$apartment_rent_sqm_now <- as.numeric(geodata$apartment_rent_sqm_now)
@@ -26,9 +25,30 @@ geodata$apartment_rent_sqm_now <- as.numeric(geodata$apartment_rent_sqm_now)
 carto <- cartogram_cont(geodata, "apartment_rent_sqm_now")
 
 # plot the cartogram
-plot <- ggplot() +
-  geom_sf(data = carto$geometry, fill = "#3F8FC5", color = "white", size = 0.5) +
-  theme_void()
+plot_cartogram <- ggplot() +
+  geom_sf(data = carto$geometry, fill = "#3F8FC5", color = "white", size = 0.2) +
+  ggtitle("[A]") +
+  theme_void() + 
+  theme(panel.border=element_rect(colour = "black", fill=NA, linewidth=0.5),
+        text=element_text(size=17, family = "Times New Roman", face="bold"),
+        plot.title=element_text(margin=margin(t=40, b=-23), hjust=0.03),
+        plot.margin = margin(1, 1, 25, 1))
 
-# save plot
-ggsave(file.path("..", "plots", "cartogram_plot.jpg"), plot, dpi = 300)
+# create non-distorted map
+plot_map <- ggplot() +
+  geom_sf(data = geodata, fill = "#3F8FC5", color = "white", size = 0.2) +
+  ggtitle("[B]") +
+  theme_void() + 
+  theme(panel.border=element_rect(colour = "black", fill=NA, linewidth=0.5),
+        text=element_text(size=17, family = "Times New Roman", face="bold"),
+        plot.title=element_text(margin=margin(t=40, b=-23), hjust=0.03),
+        plot.margin = margin(1, 1, 25, 1))
+
+
+# Combine the cartogram and map using cowplot package
+combined_plot <- cowplot::plot_grid(plot_cartogram, plot_map, ncol = 2, labels = c("", ""), hjust = -0.3)
+
+# Save plot
+ggsave(file.path("..", "plots", "cartogram_plot.jpg"), combined_plot, dpi = 300, width=10, height = 6)
+
+
