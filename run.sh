f6611a98641c78061a0deec6cb47541b4b9b779f
Anton Drasbæk <202008161@post.au.dk>
2023-06-02 14:18:03 +0200

    added analysis

--- run.sh
+++ run.sh
@@ -4,16 +4,20 @@
 source ./env/bin/activate
 
 # run cleaning of scraped data
-echo -e "[INFO:] Cleaning Data ..." # user msg 
+echo -e "[INFO:] Cleaning Rental Data ..." # user msg 
 python3 src/clean_data.py
 
 # add geo spatial data to cleaned data
-echo -e "[INFO:] Adding Geo Spatial Data..." # user msg
+echo -e "[INFO:] Adding Geo Spatial Data to Rentals..." # user msg
 python3 src/add_geodata.py
 
 # extract aggregates
 echo -e "[INFO:] Extracting Aggregates from Data ..." # user msg
 python3 src/aggregate_data.py
 
+# creating visualizations
+echo -e "[INFO:] Creating Visualizations for Analysis..." # user msg
+python3 src/analysis.py
+
 # happy msg!
-echo -e "[INFO:] Pipeline complete! ..." # user msg
+echo -e "[INFO:] Pipeline complete! You can deploy Aarhus RentMapper now from terminal (type: streamlit run app/app.py)" # user msg
